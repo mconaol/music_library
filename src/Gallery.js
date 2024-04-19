7b00ca6235323534fbebe1eaf058e66e04d086b0
Mitzy Onaolapo <rios_mit@yahoo.com>
2024-04-18 20:57:08 -0700

    initial set up complete

---
+++ src/Gallery.js
@@ -0,0 +1,14 @@
+import React, {useEffect, useState} from 'react'
+import GalleryItem from './GalleryItem'
+
+function Gallery(props){
+    return(
+        <div>
+            <GalleryItem/>
+        </div>
+    )
+    
+
+}
+
+export default Gallery 
